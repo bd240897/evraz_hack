c9481d16dc74289bce6d264d9058a88977499117
bd240897 <bd240897@yandex.ru>
2023-02-18 15:06:49 +0300

    fixed url

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -6,7 +6,7 @@ export default createStore({
   state() {
     return {
       // TODO
-      BASE_URL: " http://localhost:3004", // process.env.VUE_APP_API_URL || "http://127.0.0.1:8000", // 'http://84.201.135.220:8000', //'http://127.0.0.1:8000',
+      BASE_URL: process.env.VUE_APP_API_URL || "http://localhost:3004", // 'http://84.201.135.220:8000', //'http://127.0.0.1:8000',
       FIRST_SCREEN_URL: "/get-first-screen/",
       SECOND_SCREEN_URL: "/get-second-screen/",
     }
