d81ce01daeaef302a07ad75438a999484f3fd24c
kishik <kishik.maxim@gmail.com>
2023-02-18 13:55:15 +0300

    work with sqlalchemy

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
 from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer
@@ -30,6 +30,7 @@ class Exhauster(Base):
 
 class Colder(Base):
     __tablename__ = "colder"
+    __table_args__ = {'extend_existing': True}
     id = mapped_column(Integer, primary_key=True)
     colder_liquids: Mapped[List["ColderLiquid"]] = relationship(back_populates="colder")
     exhauster_id = mapped_column(ForeignKey("exhauster.id"))
@@ -37,7 +38,7 @@ class Colder(Base):
 
 
 class ColderLiquid(Base):
-    __tablename__ = "colder"
+    __tablename__ = "colder_liquid"
     id = mapped_column(Integer, primary_key=True)
     type = mapped_column(String(30))
     temp_after = mapped_column(Float)
@@ -94,7 +95,7 @@ class Bearing(Base):
 
 
 class Vibration(Base):
-    __tablename__ = "vibratios"
+    __tablename__ = "vibrations"
     id = mapped_column(Integer, primary_key=True)
     type = mapped_column(String(30))
     status = mapped_column(String(30))
@@ -129,3 +130,52 @@ app = FastAPI()
 def read_root():
     html_content = "<h2>Hello METANIT.COM!</h2>"
     return HTMLResponse(content=html_content)
+
+
+
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var ws = new WebSocket("ws://localhost:8000/ws");
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
+
+
+@app.get("/")
+async def get():
+    return HTMLResponse(html)
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
