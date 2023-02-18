e619dc2f75695a901267c75e2f270dbb64c51bb3
kishik <kishik.maxim@gmail.com>
2023-02-18 12:50:57 +0300

    work with sqlalchemy

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, Integer
 from typing import List
 from typing import Optional
 from sqlalchemy import ForeignKey
-from sqlalchemy import String, Float, Double
+from sqlalchemy import String, Float, Double, DateTime
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
@@ -14,6 +14,43 @@ from sqlalchemy.orm import relationship
 
 class Base(DeclarativeBase):
     pass
+
+
+class Exhauster(Base):
+    __tablename__ = "exhauster"
+    id = mapped_column(Integer, primary_key=True)
+    bearings: Mapped[List["Bearing"]] = relationship(back_populates="exhauster")
+    colder = relationship("Colder", back_populates="exhauster")
+    gas_collector = relationship("GasCollector", back_populates="exhauster")
+    valve_position = relationship("ValvePosition", back_populates="exhauster")
+    main_drive = relationship("MainDrive", back_populates="exhauster")
+    oil_system = relationship("OilSystem", back_populates="exhauster")
+    work = mapped_column(String(30))
+
+
+class Colder(Base):
+    __tablename__ = "colder"
+    id = mapped_column(Integer, primary_key=True)
+
+
+class GasCollector(Base):
+    __tablename__ = "gas_collector"
+    id = mapped_column(Integer, primary_key=True)
+
+
+class ValvePosition(Base):
+    __tablename__ = "valve_position"
+    id = mapped_column(Integer, primary_key=True)
+
+
+class MainDrive(Base):
+    __tablename__ = "main_drive"
+    id = mapped_column(Integer, primary_key=True)
+
+
+class OilSystem(Base):
+    __tablename__ = "oil_system"
+    id = mapped_column(Integer, primary_key=True)
 
 
 class Bearing(Base):
@@ -25,6 +62,8 @@ class Bearing(Base):
     temperature = relationship("Temperature", back_populates="temperature")
     vibration_status = mapped_column(String(30))
     vibrations: Mapped[List["Vibration"]] = relationship(back_populates="bearing")
+    exhauster_id = mapped_column(ForeignKey("exhauster.id"))
+    exhauster = relationship("Exhauster", back_populates="bearings")
 
 
 class Vibration(Base):
@@ -35,6 +74,7 @@ class Vibration(Base):
     value = mapped_column(Float)
     bearing_id = mapped_column(ForeignKey("bearing.id"))
     bearing = relationship("Bearing", back_populates="vibrations")
+    timestamp = mapped_column(DateTime)
 
 
 class Temperature(Base):
@@ -44,6 +84,7 @@ class Temperature(Base):
     value = mapped_column(Float)
     bearing_id = mapped_column(ForeignKey("bearing.id"))
     bearing = relationship("Bearing", back_populates="temperature")
+    timestamp = mapped_column(DateTime)
 
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./db.db"
@@ -51,19 +92,6 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./db.db"
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
-
-
-class Base(DeclarativeBase):
-    pass
-
-
-class Person(Base):
-    __tablename__ = "temperature"
-
-    id = Column(Integer, primary_key=True, index=True)
-    value = Column(String)
-    age = Column(Integer, )
-
 
 Base.metadata.create_all(bind=engine)
 
